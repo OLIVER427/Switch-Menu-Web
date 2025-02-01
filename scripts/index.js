44a715444fe69cc686f8d05cb2d1717a9f3bcf68
OLIVER427 <oliver427daminecraftplayer@gmail.com>
2025-02-01 02:43:21 -0600

    game carousel moves

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,37 @@
 // gotta have that system clock (thanks random person on stack overflow, mine would have been way too complicated)
-var time = new Date();
+let browser
+let time = new Date();
 console.log(
   time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
 );
-
 if (navigator.userAgent.includes("Chrome")) {
   //alert('Hello chromium based browser!')
   document.getElementById("profileIcon").src = "assets/images/icons/chromium.png"
+  browser = " Chromium based"
 } else if (navigator.userAgent.includes("Firefox")) {
   //alert('Hello firefox!')
   document.getElementById("profileIcon").src = "assets/images/icons/firefox.png"
+  browser = " Firefox"
 } else {
-
   document.getElementById("profileIcon").src = "assets/images/icons/unknown.png"
+  browser = "n unknown"
+
 }
+let gamesPos = 4
+document.addEventListener('keydown', (event) => {
+  if (event.key == "ArrowRight") {
+    if (gamesPos != -129) {
+      gamesPos -= 19
+    }
+      document.getElementById('gameCon').style.left = gamesPos + "vw"
+  }
+  if (event.key == "ArrowLeft") {
+    if (gamesPos != 4) {
+      gamesPos += 19
+    }
+    document.getElementById('gameCon').style.left = gamesPos + "vw"
+  }
+})
 
 function gameFrame() {
 time = new Date();
